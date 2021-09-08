686ea2697a75da4a212dcfb354b2b91e0d69f35f
Anthony Oyathelemhi <tonero91@gmail.com>
2021-09-08 20:09:21 +0100

    sprinkle a little bit of transition here and there

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Transition } from '@headlessui/react';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CreateFeedback from './components/CreateFeedback';
@@ -8,11 +9,35 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/new">
-          <CreateFeedback />
+        <Route exact path="/new">
+          <Transition
+            show
+            appear
+            key="/new"
+            enter="transition-all duration-100"
+            enterFrom="translate-y-10 opacity-50"
+            enterTo="translate-y-0 opacity-100"
+            leave="transition-all duration-150"
+            leaveFrom="translate-y-0 opacity-100"
+            leaveTo="opacity-0 translate-y-10"
+          >
+            <CreateFeedback />
+          </Transition>
         </Route>
-        <Route path="/">
-          <Home />
+        <Route exact path="/">
+          <Transition
+            show
+            appear
+            key="/"
+            enter="transition-all duration-100"
+            enterFrom="-translate-y-10 opacity-50"
+            enterTo="translate-y-0 opacity-100"
+            leave="transition-all duration-150"
+            leaveFrom="translate-y-0 opacity-100"
+            leaveTo="opacity-0 -translate-y-10"
+          >
+            <Home />
+          </Transition>
         </Route>
       </Switch>
     </Router>
